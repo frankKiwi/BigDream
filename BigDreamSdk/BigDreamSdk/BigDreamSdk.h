b2e8b7314bed6aefb7c1cdb4f3f262c3f5fb0ad8
fanrenFRank <1778907544@qq.com>
2019-04-19 23:16:23 +0800

    ppp

---
+++ BigDreamSdk/BigDreamSdk/BigDreamSdk.h
@@ -0,0 +1,20 @@
+//
+//  BigDreamSdk.h
+//  BigDreamSdk
+//
+//  Created by LWW on 2019/4/19.
+//  Copyright © 2019 LWW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BigDreamSdk.
+FOUNDATION_EXPORT double BigDreamSdkVersionNumber;
+
+//! Project version string for BigDreamSdk.
+FOUNDATION_EXPORT const unsigned char BigDreamSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigDreamSdk/PublicHeader.h>
+#import <BigDreamSdk/Dreamweaver.h>
+
+
